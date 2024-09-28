a7b6d2de1ee8266f4090de9725a9127c748245fe
achaudhary <achaudhary@beyond.com>
2024-09-28 16:05:13 -0600

    Added test cases

--- src/test/java/com/test/APITests.java
+++ src/test/java/com/test/APITests.java
@@ -1,16 +1,14 @@
 package com.test;
 
-import Utilities.ConfigReader;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.test.Responses.pojo.Base;
-import com.test.Testdata.POJO.Root;
-import com.test.Utils.Utils;
-import io.restassured.RestAssured;
-import io.restassured.http.Method;
+import com.test.testdata.Root;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
+import org.testng.asserts.SoftAssert;
+import utilities.ConfigReader;
+import com.test.requests.Request;
+import com.test.responses.Resp;
+import io.restassured.RestAssured;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -19,8 +17,6 @@ import java.io.IOException;
 
 public class APITests {
 
-    Utils utils = new Utils();
-
     @BeforeTest
     public void settestdata() throws IOException {
         ConfigReader.initialize();
@@ -28,23 +24,101 @@ public class APITests {
 
     @Test
     public void verfiyStatusCode200() throws IOException {
-        int status = utils.getCall().getStatusCode();
+        int status = getCall().getStatusCode();
         Assert.assertEquals(status,200);
+    }
+
+    @Test
+    public void testGetCall() throws IOException {
+        SoftAssert softAssert = new SoftAssert();
+
+        Response response = getCall();
+
+        Resp resp = response.body().as(Resp.class);
+
+        softAssert.assertEquals(expectedData().getPage(),
+                resp.getPage());
+
+        softAssert.assertEquals(expectedData().getPer_page(),
+                resp.getPer_page());
+
+        softAssert.assertEquals(expectedData().getTotal(),
+                resp.getTotal());
+
+        softAssert.assertEquals(expectedData().getTotal_pages(),
+                resp.getTotal_pages());
+
+        softAssert.assertEquals(expectedData().getData().get(0).getId(),
+                resp.getData().get(0).getId());
+
+        softAssert.assertEquals(expectedData().getData().get(0).getEmail(),
+                resp.getData().get(0).getEmail());
+
+        softAssert.assertEquals(expectedData().getData().get(0).getFirst_name(),
+                resp.getData().get(0).getFirst_name());
+
+        softAssert.assertEquals(expectedData().getData().get(0).getLast_name(),
+                resp.getData().get(0).getLast_name());
+
+        softAssert.assertEquals(expectedData().getData().get(0).getAvatar(),
+                resp.getData().get(0).getAvatar());
+
+        softAssert.assertAll();
+    }
+
+    //@Test
+    public void testPostCall() throws IOException {
+
+    }
+
+    //@Test
+    public void testBadRequest400(){
 
     }
 
     @Test
-    public void verifyDataForGetCall() throws IOException {
-        Response response = utils.getCall();
+    public void testNotFound404(){
 
-        Base base = response.body().as(Base.class);
+        String badUrl = "https://reqres.in/ai/users";
+        Response response = RestAssured.given()
+                .header("Content-Type",
+                        "application/json")
+                .baseUri(badUrl).get();
 
-        Assert.assertEquals(base.getName(), utils.expectedData().getName());
+        Assert.assertEquals(response.statusCode(), 404);
     }
 
 
+    public String request() throws IOException {
 
+        ObjectMapper objectMapper = new ObjectMapper();
 
+        Request request = new Request();
 
-    
+        /*
+        req.getData().setPrice(1849.99);
+        req.getData().setYear(2020);
+        req.getData().setHardDiskSize("1 TB");
+        req.getData().setCpuModel("Intel Core i9");
+        */
+        return   objectMapper.writeValueAsString(request);
+    }
+
+    public Response getCall(){
+        Response response = RestAssured.given()
+                .baseUri(ConfigReader.get("baseUrl"))
+                .queryParam("page",1).get();
+
+        return response;
+    }
+
+    public Root expectedData() throws IOException {
+
+        File file = new File("src/test" +
+                "/resources/json/testdata.json");
+        ObjectMapper objectMapper = new ObjectMapper();
+        Root root = objectMapper.readValue(file, Root.class);
+
+        return root;
+    }
 }
---
+++ src/test/java/com/test/responses/Resp.java
@@ -0,0 +1,65 @@
+package com.test.responses;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.ArrayList;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Resp {
+    public int page;
+    public int per_page;
+    public int total;
+    public int total_pages;
+    public Support support;
+    public ArrayList<Data> data;
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getPer_page() {
+        return per_page;
+    }
+
+    public void setPer_page(int per_page) {
+        this.per_page = per_page;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public int getTotal_pages() {
+        return total_pages;
+    }
+
+    public void setTotal_pages(int total_pages) {
+        this.total_pages = total_pages;
+    }
+
+    public ArrayList<Data> getData() {
+        return data;
+    }
+
+    public void setData(ArrayList<Data> data) {
+        this.data = data;
+    }
+
+    public Support getSupport() {
+        return support;
+    }
+
+    public void setSupport(Support support) {
+        this.support = support;
+    }
+
+
+}
